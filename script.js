b7756f8fe3735e5e8d7274e236b2e13a54694905
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-05-04 12:20:22 -0230

    Added pages and working on styling

---
+++ script.js
@@ -0,0 +1,9 @@
+function showFilmPage() {
+    window.location = "films.html";
+  }
+function showListPage() {
+    window.location = "list.html";
+  }
+  function showJournalPage() {
+    window.location = "journal.html";
+  }
