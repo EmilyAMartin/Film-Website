c3d31bb6555e89f2558de9c0efd8b9e96b54498a
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-05-30 11:41:22 -0230

    Working on journal page, added new entry pop up

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 //Home Page//
-function showHomePage(){
-    window.location = "index.html";
+function showHomePage() {
+  window.location = "index.html";
 }
-function showFilmPage() {
-    window.location = "films.html";
-  }
+function showFavoritesPage() {
+  window.location = "favorites.html";
+}
 function showListPage() {
-    window.location = "list.html";
-  }
-  function showJournalPage() {
-    window.location = "journal.html";
-  }
+  window.location = "list.html";
+}
+function showJournalPage() {
+  window.location = "journal.html";
+}
 //Journal Page//
+function showJournalForm() {
+  let journalForm = document.getElementById("journalForm");
+  if (journalForm.style.display === "none") {
+    journalForm.style.display = "block";
+  } else {
+    journalForm.style.display = "none";
+  }
+}
 function addRating(){
-  
+
+}
+function newEntry(){
+  document.getElementById("newEntry").style.display = "block";
+  document.getElementById("journalForm").style.display = "none";
 }
 //Film Page//
 //List Page//
